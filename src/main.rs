c3ff7decf3985ff720f8f2dde53fea689e281b28
Graham Keenan <graham.keenan@outlook.com>
2024-11-01 12:15:37 +0000

    send correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use tokio::net::TcpListener;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:9092").await?;
 
-    while let Ok((_client, _)) = listener.accept().await {
-        println!("Client accepted");
+    loop {
+        match listener.accept().await {
+            Ok((mut client, _)) => {
+                let message_size: i32 = 12;
+                let correlation_id: i32 = 7;
+                let mut response = vec![];
+                response.extend(message_size.to_be_bytes());
+                response.extend(correlation_id.to_be_bytes());
+                client.write(&response).await?;
+            }
+            Err(e) => anyhow::bail!(e),
+        }
     }
-
-    Ok(())
 }
