1299d48047a073de16b1e051ca6aed2b889039e5
Graham Keenan <graham.keenan@outlook.com>
2025-12-20 18:32:33 +0000

    send correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use std::net::TcpListener;
+use anyhow::{Context, Result};
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut response = Vec::with_capacity(8);
+                response.extend(0i32.to_be_bytes());
+                response.extend(7i32.to_be_bytes());
+                stream
+                    .write(&response)
+                    .context("sending response to client")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
