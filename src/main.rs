f354f020dc3b74e721871f68e98744dcea499ed1
Graham Keenan <graham.keenan@outlook.com>
2025-12-20 19:25:57 +0000

    parse api version

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,28 @@ use std::{
     net::TcpListener,
 };
 
+struct RequestHeader {
+    api_key: i16,
+    api_version: i16,
+    correlation_id: i32,
+}
+
+impl RequestHeader {
+    pub fn parse(req: &mut BytesMut) -> Result<Self> {
+        let api_key = i16::from_be_bytes(req[..2].try_into()?);
+        req.advance(2);
+        let api_version = i16::from_be_bytes(req[..2].try_into()?);
+        req.advance(2);
+        let correlation_id = i32::from_be_bytes(req[..4].try_into()?);
+
+        Ok(Self {
+            api_key,
+            api_version,
+            correlation_id,
+        })
+    }
+}
+
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
@@ -15,15 +37,18 @@ fn main() -> Result<()> {
                 stream.read(&mut buf).context("read from client")?;
                 let _msg_size = i32::from_be_bytes(buf[..4].try_into()?);
                 buf.advance(4);
-                let _api_key = i16::from_be_bytes(buf[..2].try_into()?);
-                buf.advance(2);
-                let _api_version = i16::from_be_bytes(buf[..2].try_into()?);
-                buf.advance(2);
-                let correlation_id = i32::from_be_bytes(buf[..4].try_into()?);
+                let header = RequestHeader::parse(&mut buf)?;
+
+                let error_code = if header.api_version < 0 || header.api_key > 4 {
+                    35i16.to_be_bytes()
+                } else {
+                    0i16.to_be_bytes()
+                };
 
                 let mut response = Vec::with_capacity(8);
                 response.extend(0i32.to_be_bytes());
-                response.extend(correlation_id.to_be_bytes());
+                response.extend(header.correlation_id.to_be_bytes());
+                response.extend(error_code);
                 stream.write(&response).context("write to client")?;
             }
             Err(e) => {
